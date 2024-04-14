a8067d99ede65c0d551c4c7e3b3c639c43199b71
Abdi <ab_cs2048@yahoo.com>
2024-04-14 21:03:14 +0300

    made cors allow frontend to read backend

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 // 
-app.use(cors({credentials:true, origin:['http://localhost:3000', "https://mern_blog_app.onrender.com"],}));
+app.use(cors({credentials:true, origin:['http://localhost:3000', "https://blogapp-client-7iyu.onrender.com"],}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
